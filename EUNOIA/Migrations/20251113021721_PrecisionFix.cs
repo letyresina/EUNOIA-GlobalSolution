6b942b627ca66e7d8aa8c5295f851918e7eaf3bb
letyresinaa <leticiaresina@gmail.com>
2025-11-12 23:18:46 -0300

    fix: precision em decimais

---
+++ EUNOIA/Migrations/20251113021721_PrecisionFix.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EUNOIA.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrecisionFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ConfidenceScore",
+                table: "EmotionSessions",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Intensity",
+                table: "Emotion",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Duration",
+                table: "Emotion",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ConfidenceScore",
+                table: "EmotionSessions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Intensity",
+                table: "Emotion",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Duration",
+                table: "Emotion",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+        }
+    }
+}
